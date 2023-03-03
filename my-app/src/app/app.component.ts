78701dd5e25b3fa37f1ed391d766b70a6a6d57a1
Mark Harmon <neothej1sec@gmail.com>
2023-03-02 18:23:49 -0600

    two way binding

--- my-app/src/app/app.component.ts
+++ my-app/src/app/app.component.ts
@@ -1,5 +1,6 @@
+import { NgIf } from '@angular/common';
 import { Component } from '@angular/core';
-
+import { FormsModule } from '@angular/forms';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -8,6 +9,7 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title: string;
   imgSrc: string = "https://static.onecms.io/wp-content/uploads/sites/47/2021/04/13/kitten-pounce-158906394-2000.jpg";
+  favAnimal: string = "kitty";
   constructor(){
     this.title = "Neos-1st-Angular-App"
   }

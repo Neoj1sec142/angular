6e9622e2574769b39b210e9eb3a1e7689d7a5f9f
Mark Harmon <neothej1sec@gmail.com>
2023-03-06 22:52:06 -0600

    notes

--- my-app/src/app/app.component.ts
+++ my-app/src/app/app.component.ts
@@ -1,18 +1,45 @@
 import { NgIf } from '@angular/common';
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+
+interface Pokemon {
+  id: number,
+  name: string,
+  type: string,
+  isCool: boolean
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  pokemonName: string = "";
+  pokemons: Pokemon[] = [{
+    id: 1,
+    name: 'pikachu',
+    type: 'electric',
+    isCool: false
+  },{
+    id: 2,
+    name: 'squirtle',
+    type: 'water',
+    isCool: true
+  },{
+    id: 3,
+    name: 'charmander',
+    type: 'fire',
+    isCool: false
+  },{
+    id: 4,
+    name: 'bulbasaur',
+    type: 'grass',
+    isCool: true
+  }]
+  
   
   constructor(){
     
   }
-  handleClick(value: any){
-    console.log(value)
-  }
+  
 }

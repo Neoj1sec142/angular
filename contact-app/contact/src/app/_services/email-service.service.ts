48b2975c4472c033807d206936cfde9fbb94d247
Mark Harmon <neothej1sec@gmail.com>
2023-03-18 15:21:53 -0500

    finished adding email and social services:

--- contact-app/contact/src/app/_services/email-service.service.ts
+++ contact-app/contact/src/app/_services/email-service.service.ts
@@ -1,10 +1,27 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-
+import { Observable } from 'rxjs';
+import { ContactEmail } from '../_models/email';
+const EMAILS_API = 'http://localhost:8000/contact-emails'
 @Injectable({
   providedIn: 'root'
 })
 export class EmailServiceService {
 
-constructor() { }
+constructor(private http: HttpClient) { }
+
+
+
+addEmail(email: ContactEmail): Observable<ContactEmail> {
+  return this.http.post<ContactEmail>(`${EMAILS_API}/`, email);
+}
+
+updateEmail(id: number, email: ContactEmail): Observable<ContactEmail> {
+  return this.http.put<ContactEmail>(`${EMAILS_API}/${id}/`, email);
+}
+
+deleteEmail(id: number): Observable<any> {
+  return this.http.delete(`${EMAILS_API}/${id}/`);
+}
 
 }
--- contact-app/contact/src/app/_services/social-service.service.ts
+++ contact-app/contact/src/app/_services/social-service.service.ts
@@ -1,10 +1,25 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-
+import { Observable } from 'rxjs';
+import { ContactSocial } from '../_models/social';
+const SOCIALS_API = 'http://localhost:8000/contact-socials'
 @Injectable({
   providedIn: 'root'
 })
 export class SocialServiceService {
 
-constructor() { }
+constructor(private http: HttpClient) { }
+
+addEmail(social: ContactSocial): Observable<ContactSocial> {
+  return this.http.post<ContactSocial>(`${SOCIALS_API}/`, social);
+}
+
+updateEmail(id: number, social: ContactSocial): Observable<ContactSocial> {
+  return this.http.put<ContactSocial>(`${SOCIALS_API}/${id}/`, social);
+}
+
+deleteEmail(id: number): Observable<any> {
+  return this.http.delete(`${SOCIALS_API}/${id}/`);
+}
 
 }
